b8f3a621bcbb0e3e1fe11b7e484a8276cd3bd1ed
Ricardo Rodriguez <lucario.the.hero@gmail.com>
2026-03-25 21:53:25 +0000

    changes

--- Main.java
+++ Main.java
@@ -4,9 +4,49 @@ class Main
 {
   public static void main(String[] args)
   {
+    int one = 0;
+    int two = 0;
+    int three = 0;
+    int four = 0;
+    int five = 0;
+    int six = 0;
+
+
+    for (int i = 1; i <= 100; i++){
     // Example calling the roll() method
-    System.out.println("Roll: " + roll());
+    int currentRoll = roll();
+    System.out.println("Roll: "+ i + ": [" + currentRoll + "]");
+    if (currentRoll == 1){
+      one++;
+    }
+    if (currentRoll == 2){
+      two++;
+    }
+    if (currentRoll == 3){
+      three++;
+    }
+    if (currentRoll == 4){
+      four++;
+    }
+    if (currentRoll == 5){
+      five++;
+    }
+    if (currentRoll == 6){
+      six++;
+    }
+    }
+
+    System.out.println("");
+    System.out.println("Final Role Counts: ");
+    System.out.println("[1]: " + one + " times");
+    System.out.println("[2]: " + two + " times");
+    System.out.println("[3]: " + three + " times");
+    System.out.println("[4]: " + four + " times");
+    System.out.println("[5]: " + five + " times");
+    System.out.println("[6]: " + six + " times");
+
   }
+
   
   // Random is a class we can use to generate random numbers
   // You do not need to change anything in the method below
